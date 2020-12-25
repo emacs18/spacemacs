d78def4db551b1c4e2dccc0f6be8e5320d22bd91
Richard Kim <emacs18@gmail.com>
2020-12-25 09:58:46 -0800

    added ":straight nil" to use-package forms that straight cannot handle

--- layers/+spacemacs/spacemacs-defaults/packages.el
+++ layers/+spacemacs/spacemacs-defaults/packages.el
@@ -149,6 +149,7 @@
 
 (defun spacemacs-defaults/init-dired-x ()
   (use-package dired-x
+    :straight nil
     :commands (dired-jump
                dired-jump-other-window
                dired-omit-mode)))
@@ -224,6 +225,7 @@
 
 (defun spacemacs-defaults/init-image-mode ()
   (use-package image-mode
+    :straight nil
     :defer t
     :init
     (progn
