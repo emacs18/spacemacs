464024f8c0425b8105c60192d3eb948a598186d6
Richard Kim <emacs18@gmail.com>
2020-12-25 09:58:46 -0800

    added ":straight nil" to use-package forms that straight cannot handle

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -117,6 +117,7 @@
 
 (defun org/init-ob ()
   (use-package ob
+    :straight nil
     :defer t
     :init
     (progn
@@ -484,6 +485,7 @@ Will work on both org-mode and any mode that accepts plain html."
 
 (defun org/init-org-agenda ()
   (use-package org-agenda
+    :straight nil
     :defer t
     :init
     (progn
@@ -657,6 +659,7 @@ Headline^^            Visit entry^^               Filter^^                    Da
 
 (defun org/init-org-expiry ()
   (use-package org-expiry
+    :straight nil
     :commands (org-expiry-insinuate
                org-expiry-deinsinuate
                org-expiry-insert-created
