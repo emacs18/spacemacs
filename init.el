386e4c5dcc010fb2a683905c74944d5a52b45770
Richard Kim <emacs18@gmail.com>
2022-10-08 23:44:47 -0700

    init.el: set dotspacemacs-filepath to .spacemacs.template

--- init.el
+++ init.el
@@ -27,6 +27,11 @@
 ;; Avoid garbage collection during startup.
 ;; see `SPC h . dotspacemacs-gc-cons' for more info
 (defconst emacs-start-time (current-time))
+(setq spacemacs-start-directory (file-name-directory load-file-name))
+
+;; Load following rather than ~/.spacemacs.
+(setq dotspacemacs-filepath (expand-file-name "core/templates/.spacemacs.template" spacemacs-start-directory))
+
 (setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
 (load (concat (file-name-directory load-file-name)
               "core/core-versions")
